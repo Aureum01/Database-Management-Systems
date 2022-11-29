e5ee63858f217b749659605f1195f7353516bba7
Aureum01 <71108402+Aureum01@users.noreply.github.com>
2022-11-28 22:33:25 -0500

    Create Value 2.sql
    IN Microsoft Access you may get "Microsoft Access can't append all the records in the append query" but ignore it and press yes to append the record into the table. Make sure the table isn't open as to not get an error. Since this is the second record, the [example] helps add another record.

---
+++ Assessment/Value 2.sql
@@ -0,0 +1,2 @@
+INSERT INTO Office (OfficeNum, [OfficeName], [OfficeAddress], [Area], [City], [State], [Zip]) 
+    VALUES (1, 'StayWell-Georgetown', '986 S. Madison Rd', 'Georgetown', 'Seattle', 'WA', '98108') 
