635d1041f33dad52454b8914beeffcd603e38755
Aureum01 <71108402+Aureum01@users.noreply.github.com>
2022-11-28 22:34:04 -0500

    Create value1.sql
    First added record

---
+++ Assessment/value1.sql
@@ -0,0 +1,2 @@
+INSERT INTO Office ( OfficeNum, OfficeName, OfficeAddress, Area, City, State, Zip )
+VALUES (1, 'StayWell-Colombia City', '1135 N. Wells Ave', 'Columbia City', 'Seattle', 'WA', '98118');
