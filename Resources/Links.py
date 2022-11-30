2ac226b23afcee84b6c9e0e2179f39325aa42723
Aureum01 <71108402+Aureum01@users.noreply.github.com>
2022-11-29 19:44:22 -0500

    Create Links.py

---
+++ Resources/Links.py
@@ -0,0 +1,11 @@
+# https://support.microsoft.com/en-us/office/apply-criteria-to-text-values-202548b6-b065-4387-800b-2456a7099bca
+--------------------------------------------------------------------------------------------------------------
+# https://support.microsoft.com/en-us/topic/a-potentially-dangerous-macro-has-been-blocked-0952faa0-37e7-4316-b61d-5b5ed6024216
+--------------------------------------------------------------------------------------------------------------
+# https://learn.microsoft.com/en-us/office/client-developer/access/desktop-database-reference/create-view-statement-microsoft-access-sql
+--------------------------------------------------------------------------------------------------------------
+# https://stackoverflow.com/questions/101779/the-action-or-event-has-been-blocked-by-disabled-mode
+--------------------------------------------------------------------------------------------------------------
+# https://support.microsoft.com/en-us/office/use-like-criterion-to-locate-data-65b07c8a-b314-435a-8b48-2b911856d4f9
+--------------------------------------------------------------------------------------------------------------
+# https://www.fmsinc.com/microsoftaccess/query/outer-join/index.html
